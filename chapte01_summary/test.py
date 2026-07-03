8068afb85d99099ed939ff6f6e1ac41fbc2e4327
矢吹丈 <2865874934@qq.com>
2026-07-03 21:42:50 +0900

    ->(2026.7.3)尝试接入智谱和阿里百炼云的ai模型

--- chapte01_summary/test.py
+++ chapte01_summary/test.py
@@ -1,8 +1,11 @@
 import langchain
 import pydantic
+from langchain_community.chat_models import ChatTongyi
 
 print(langchain.__version__)
 
 
 print(pydantic.__version__)
 print("pydantic 导入成功！")
+
+ChatTongyi
