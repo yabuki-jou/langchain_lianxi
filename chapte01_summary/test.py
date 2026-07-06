a1f5049bb40b4dab15bb8c9604422b2fcaab6d39
矢吹丈 <2865874934@qq.com>
2026-07-06 18:46:13 +0900

    ->(2026.7.3)尝试接入智谱和阿里百炼云的ai模型

--- chapte01_summary/test.py
+++ chapte01_summary/test.py
@@ -1,11 +1,17 @@
-import langchain
-import pydantic
-from langchain_community.chat_models import ChatTongyi
 
-print(langchain.__version__)
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
 
-
-print(pydantic.__version__)
-print("pydantic 导入成功！")
-
-ChatTongyi
+load_dotenv(override=True)
+DEEPSEEK_API_KEY= os.getenv("DEEPSEEK_API_KEY")
+DEEPSEEK_BASE_URL= os.getenv("DEEPSEEK_BASE_URL")
+llmOpenai = init_chat_model(
+    model="deepseek:deepseek-v4-flash",
+    api_key=DEEPSEEK_API_KEY,
+    base_url=DEEPSEEK_BASE_URL,
+    temperature=0
+)
+for i in range(3):
+    response = llmOpenai.invoke("写一个十个字的小说大纲")
+    print(response.content)
